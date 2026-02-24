655ebdf803b88e5f80ce76c85c9a76b05c0d32a7
Akashkd007 <akashkdoddamani12@gmail.com>
2026-02-24 23:54:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,16 +4,6 @@ module "ec2_server" {
   instance_type  = "t2.small"
   instance_name  = "Module-EC2"
 }
-module "s3_bucket" {
-  source = "./modules/s3"
 
-  bucket_name        = "akash-demo-${terraform.workspace}-123456"
-  environment        = terraform.workspace
-  versioning_enabled = true
 
-  tags = {
-    Project = "Terraform-Demo"
-    Owner   = "Akash"
-  }
-}
 
